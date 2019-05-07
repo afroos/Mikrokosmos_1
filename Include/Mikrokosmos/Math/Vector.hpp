9be80a8b3f2516eaaa47dbc7f452264cd7ef846d
André Roos <andrefroos@gmail.com>
2019-05-06 23:47:37 -0300

    Added Vector class.

---
+++ Include/Mikrokosmos/Math/Vector.hpp
@@ -0,0 +1,231 @@
+#ifndef MIKROKOSMOS_MATH_VECTOR_HPP
+#define MIKROKOSMOS_MATH_VECTOR_HPP
+
+#include <algorithm>
+#include <array>
+#include <numeric>
+#include <Mikrokosmos/Common/Real.hpp>
+
+namespace Mikrokosmos
+{
+	template <typename T, std::size_t N>
+	class Vector
+	{
+	public:
+
+		using SizeType = std::size_t;
+		using Reference = T&;
+		using ConstReference = const T&;
+		using Iterator = T*;
+		using ConstIterator = const T*;
+
+		constexpr Vector() noexcept = default;
+
+		template<typename... Args, typename = std::enable_if_t<sizeof...(Args) == N>>
+		constexpr Vector(Args&&... args) noexcept
+			: coords{ {args...} }
+		{
+		}
+
+		static constexpr Vector zero() noexcept { return Vector(); }
+
+		template<typename = std::enable_if_t<(N > 0)>>
+		static constexpr Vector unitX() noexcept { return unitAt(0); }
+
+		template<typename = std::enable_if_t<(N > 1)>>
+		static constexpr Vector unitY() noexcept { return unitAt(1); }
+
+		template<typename = std::enable_if_t<(N > 2)>>
+		static constexpr Vector unitZ() noexcept { return unitAt(2); }
+
+		constexpr ConstIterator begin() const noexcept { return coords.data(); }
+		constexpr Iterator begin() noexcept { return coords.data(); }
+
+		constexpr ConstIterator end() const noexcept { return coords.data() + coords.size(); }
+		constexpr Iterator end() noexcept { return coords.data() + coords.size(); }
+
+		constexpr ConstReference operator[](SizeType index) const noexcept
+		{
+			assert(index < coords.size());
+			return coords[index];
+		}
+
+		constexpr Reference operator[](SizeType index) noexcept
+		{
+			assert(index < coords.size());
+			return coords[index];
+		}
+
+		template<typename = std::enable_if_t<(N > 0)>>
+		constexpr T x() const noexcept { return coords[0]; }
+
+		template<typename = std::enable_if_t<(N > 1)>>
+		constexpr T y() const noexcept { return coords[1]; }
+
+		template<typename = std::enable_if_t<(N > 2)>>
+		constexpr T z() const noexcept { return coords[2]; }
+
+		template<typename = std::enable_if_t<(N > 0)>>
+		void setX(T value) noexcept { coords[0] = value; }
+
+		template<typename = std::enable_if_t<(N > 1)>>
+		void setY(T value) noexcept { coords[1] = value; }
+
+		template<typename = std::enable_if_t<(N > 2)>>
+		void setZ(T value) noexcept { coords[2] = value; }
+
+		constexpr Vector& operator+=(const Vector& rhs) noexcept
+		{
+			std::transform(begin(), end(), rhs.begin(), begin(), std::plus<Real>());
+			return *this;
+		}
+
+		constexpr Vector& operator-=(const Vector& rhs) noexcept
+		{
+			std::transform(begin(), end(), rhs.begin(), begin(), std::minus<Real>());
+			return *this;
+		}
+
+		constexpr Vector& operator*=(const Vector& rhs) noexcept
+		{
+			std::transform(begin(), end(), rhs.begin(), begin(), std::multiplies<Real>());
+			return *this;
+		}
+
+		constexpr Vector& operator/=(const Vector& rhs) noexcept
+		{
+			std::transform(begin(), end(), rhs.begin(), begin(), std::divides<Real>());
+			return *this;
+		}
+
+		constexpr Vector& operator*=(const T& rhs) noexcept
+		{
+			for (auto& coord : coords) { coord *= rhs; }
+			return *this;
+		}
+
+		constexpr Vector& operator/=(const T& rhs) noexcept
+		{
+			for (auto& coord : coords) { coord /= rhs; }
+			return *this;
+		}
+
+	private:
+		std::array<T, N> coords{ };
+
+		static constexpr Vector unitAt(SizeType index) noexcept
+		{
+			Vector v;
+			v[index] = Real{ 1 };
+			return v;
+		}
+	};
+
+	template <typename T>
+	using Vector2 = Vector<T, 2>;
+
+	template <typename T>
+	using Vector3 = Vector<T, 3>;
+
+	using Real2 = Vector2<Real>;
+	using Real3 = Vector3<Real>;
+
+	template <typename T, std::size_t N>
+	inline constexpr bool operator==(const Vector<T, N>& lhs, const Vector<T, N>& rhs) noexcept
+	{
+		return std::equal(lhs.begin(), lhs.end(), rhs.begin());
+	}
+
+	template <typename T, std::size_t N>
+	inline constexpr bool operator!=(const Vector<T, N>& lhs, const Vector<T, N>& rhs) noexcept
+	{
+		return !(rhs == lhs);
+	}
+
+	template <typename T, std::size_t N>
+	inline constexpr Vector<T, N> operator+(const Vector<T, N>& v) noexcept
+	{
+		return v;
+	}
+
+	template <typename T, std::size_t N>
+	inline constexpr Vector<T, N> operator-(const Vector<T, N>& v) noexcept
+	{
+		Vector<T, N> result;
+		std::transform(v.begin(), v.end(), result.begin(), std::negate<Real>());
+		return result;
+	}
+
+	template <typename T, std::size_t N>
+	inline constexpr Vector<T, N> operator+(Vector<T, N> lhs, const Vector<T, N>& rhs) noexcept
+	{
+		return lhs += rhs;
+	}
+
+	template <typename T, std::size_t N>
+	inline constexpr Vector<T, N> operator-(Vector<T, N> lhs, const Vector<T, N>& rhs) noexcept
+	{
+		return lhs -= rhs;
+	}
+
+	template <typename T, std::size_t N>
+	inline constexpr Vector<T, N> operator*(Vector<T, N> lhs, const Vector<T, N>& rhs) noexcept
+	{
+		return lhs *= rhs;
+	}
+
+	template <typename T, std::size_t N>
+	inline constexpr Vector<T, N> operator/(Vector<T, N> lhs, const Vector<T, N>& rhs) noexcept
+	{
+		return lhs /= rhs;
+	}
+
+	template <typename T, std::size_t N>
+	inline constexpr Vector<T, N> operator*(Vector<T, N> lhs, const T& rhs) noexcept
+	{
+		return lhs *= rhs;
+	}
+
+	template <typename T, std::size_t N>
+	inline constexpr Vector<T, N> operator*(const T& lhs, Vector<T, N> rhs) noexcept
+	{
+		return rhs *= lhs;
+	}
+
+	template <typename T, std::size_t N>
+	inline constexpr Vector<T, N> operator/(Vector<T, N> lhs, const T& rhs) noexcept
+	{
+		return lhs /= rhs;
+	}
+
+	template <typename T, std::size_t N>
+	std::ostream& operator<<(std::ostream& os, const Vector<Real, N>& v)
+	{
+		os << '(';
+		std::copy(v.begin(), v.end(), std::ostream_iterator<T>(os, ", "));
+		os << "\b\b)";
+		return os;
+	}
+
+	template <typename T, std::size_t N>
+	inline constexpr T dot(const Vector<T, N>& lhs, const Vector<T, N>& rhs) noexcept
+	{
+		return static_cast<T>(std::inner_product(lhs.begin(), lhs.end(), rhs.begin(), 0.0));
+	}
+
+	template <typename T, std::size_t N>
+	inline constexpr T lengthSquared(const Vector<T, N>& v) noexcept
+	{
+		return dot(v, v);
+	}
+
+	template <typename T, std::size_t N>
+	inline constexpr T length(const Vector<T, N>& v) noexcept
+	{
+		return std::sqrt(lengthSquared(v));
+	}
+
+}
+
+
+#endif
---
+++ Test/VectorTest.cpp
@@ -0,0 +1,152 @@
+#include "catch.hpp"
+#include <Mikrokosmos/Math/Vector.hpp>
+
+using namespace Mikrokosmos;
+
+TEST_CASE("Vector: Storage size") 
+{
+	REQUIRE(sizeof(Real3) == 3 * sizeof(Real));
+}
+
+TEST_CASE("Vector: Construction")
+{
+	Vector<Real, 0> v0;
+	Vector2<Real> v1;
+	Vector2<Real> v2{};
+	Vector2<Real> v3{ 0.0, 0.0 };
+	Vector<Real, 5> v4{ -3.0, -1.5, 0.0, 1.5, 3.0 };
+	Vector<Real, 5> v5(-3.0, -1.5, 0.0, 1.5, 3.0);
+	Vector<Real, 5> v6({ -3.0, -1.5, 0.0, 1.5, 3.0 });
+	Vector<Real, 5> v7 = { -3.0, -1.5, 0.0, 1.5, 3.0 };
+	Vector3<int> v8{ -1, 0, 1 };
+	auto v9{ v8 };
+	auto v10{ std::move(v8) };
+
+	REQUIRE(v1 == v2);
+	REQUIRE(v2 == v3);
+	REQUIRE(v4 == v5);
+	REQUIRE(v5 == v6);
+	REQUIRE(v6 == v7);
+	REQUIRE(v8 == v9);
+	REQUIRE(v8 == v10);
+}
+
+TEST_CASE("Vector: Constants")
+{
+	REQUIRE(Real3::zero()  == Real3{ 0.0, 0.0, 0.0 });
+	REQUIRE(Real3::unitX() == Real3{ 1.0, 0.0, 0.0 });
+	REQUIRE(Real3::unitY() == Real3{ 0.0, 1.0, 0.0 });
+	REQUIRE(Real3::unitZ() == Real3{ 0.0, 0.0, 1.0 });
+}
+
+TEST_CASE("Vector: Assignment")
+{
+	Real2 v1{ -1.0, -1.0 };
+	Real2 v2{ 0.0, 1.0 };
+	v1 = v2;
+	v2 = { 2.0, 3.0 };
+
+	REQUIRE(v1 == Real2{ 0.0, 1.0 });
+	REQUIRE(v2 == Real2{ 2.0, 3.0 });
+}
+
+TEST_CASE("Vector: Element access")
+{
+	Real3 v1{ -1.0, -0.5, 1.0 };
+	Real3 v2;
+	Real3 v3;
+
+	v2[0] = v1[0];
+	v2[1] = v1[1];
+	v2[2] = v1[2];
+
+	v3.setX(v2.x());
+	v3.setY(v2.y());
+	v3.setZ(v2.z());
+
+	REQUIRE(v2 == Real3{ -1.0, -0.5, 1.0 });
+	REQUIRE(v3 == v2);
+}
+
+TEST_CASE("Vector: Arithmetic operators")
+{
+	Real3 v1{ -1.0, -2.0, 3.0 };
+	Real3 v2{ 1.0, 1.0, 1.0 };
+	Real3 v3;
+
+	v3 = +v1;
+	REQUIRE(v3 == v1);
+
+	v3 = -v1;
+	REQUIRE(v3 == Real3{ 1.0, 2.0, -3.0 });
+
+	v3 += Real3{ -1.0, -1.0, 1.0 };
+	REQUIRE(v3 == Real3{ 0.0, 1.0, -2.0 });
+
+	v3 -= Real3{ 0.5, 0.5, 0.5 };
+	REQUIRE(v3 == Real3{ -0.5, 0.5, -2.5 });
+
+	v3 *= Real3{ -1.0, 2.0, 3.0 };
+	REQUIRE(v3 == Real3{ 0.5, 1.0, -7.5 });
+
+	v3 /= Real3{ 0.5, 1.0, -2.5 };
+	REQUIRE(v3 == Real3{ 1.0, 1.0, 3.0 });
+
+	v3 *= 3.0;
+	REQUIRE(v3 == Real3{ 3.0, 3.0, 9.0 });
+
+	v3 /= -1.5;
+	REQUIRE(v3 == Real3{ -2.0, -2.0, -6.0 });
+
+	v3 = v1 + v2;
+	REQUIRE(v3 == Real3{ 0.0, -1.0, 4.0 });
+
+	v3 = v1 - v2;
+	REQUIRE(v3 == Real3{ -2.0, -3.0, 2.0 });
+
+	v3 = v1 * 1.5;
+	REQUIRE(v3 == Real3{ -1.5, -3.0, 4.5 });
+
+	v3 = -1.5 * v1;
+	REQUIRE(v3 == Real3{ 1.5, 3.0, -4.5 });
+
+	v3 = v2 / 0.5;
+	REQUIRE(v3 == Real3{ 2.0, 2.0, 2.0 });
+}
+
+TEST_CASE("Vector: Dot product")
+{
+	Real3 v1{ 1.0, 0.0, 0.0 };
+	Real3 v2{ 0.0, 1.0, 0.0 };
+	Real3 v3{ -1.0, 0.0, 0.0 };
+	Real3 v4{ 0.0, -1.0, 0.0 };
+	Real3 v5{ 1.0, 2.0, 3.0 };
+	Real3 v6{ 4.0, 5.0, 6.0 };
+
+	REQUIRE(dot(v1, v1) == Approx(1.0));
+	REQUIRE(dot(v1, v2) == Approx(0.0));
+	REQUIRE(dot(v1, v3) == Approx(-1.0));
+	REQUIRE(dot(v1, v4) == Approx(0.0));
+	REQUIRE(dot(v5, v6) == Approx(32.0));
+}
+
+TEST_CASE("Vector: Length")
+{
+	Real2 v1{ 0.0, 0.0 };
+	Real2 v2{ 1.0, 0.0 };
+	Real2 v3{ 0.0, 1.0 };
+	Real2 v4{ 3.0, 4.0 };
+	Real2 v5{ -3.0, -4.0 };
+
+	REQUIRE(length(v1) == Approx(0.0));
+	REQUIRE(length(v2) == Approx(1.0));
+	REQUIRE(length(v2) == length(v3));
+	REQUIRE(length(v4) == Approx(5.0));
+	REQUIRE(length(v4) == length(v5));
+
+	REQUIRE(lengthSquared(v1) == Approx(0.0));
+	REQUIRE(lengthSquared(v2) == Approx(1.0));
+	REQUIRE(lengthSquared(v2) == lengthSquared(v3));
+	REQUIRE(lengthSquared(v4) == Approx(25.0));
+	REQUIRE(lengthSquared(v4) == lengthSquared(v5));
+}
